fae3aa7a50ae33d9fe6af918411c46a908904cb4
Wian Vos <wian.vos@kvk.nl>
2014-03-19 15:05:02 +0100

    added proxy_url=undef to rvm::passenger::gem

--- manifests/passenger/gem.pp
+++ manifests/passenger/gem.pp
@@ -1,4 +1,4 @@
-class rvm::passenger::gem($ruby_version, $version, $proxy_url) {
+class rvm::passenger::gem($ruby_version, $version, $proxy_url = undef ) {
   $ruby_version_only = regsubst($ruby_version,'([^@]+)(@(.+))?','\1')
   rvm_gem {
     "passenger":
