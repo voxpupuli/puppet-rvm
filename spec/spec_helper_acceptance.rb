4ec96a5bf62d5c4c8aa2e96843a59d5df627ec31
Tim Meusel <tim@bastelfreak.de>
2020-12-04 21:24:58 +0100

    update spec_helper_acceptance.rb

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,50 +1,7 @@
-require 'puppet'
-require 'beaker-rspec/spec_helper'
-require 'beaker-rspec/helpers/serverspec'
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-# overriding puppet installation for the RedHat family distros due to
-# puppet breakage >= 3.5
-def install_puppet(host)
-  host['platform'] =~ %r{(fedora|el)-(\d+)}
-  if host['platform'] =~ %r{(fedora|el)-(\d+)}
-    safeversion = '3.4.2'
-    platform = Regexp.last_match(1)
-    relver = Regexp.last_match(2)
-    on host, "rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-#{platform}-#{relver}.noarch.rpm"
-    on host, "yum install -y puppet-#{safeversion}"
-  else
-    super()
-  end
-end
-
-RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-
-  c.before do
-    Puppet::Util::Log.level = :warning
-    Puppet::Util::Log.newdestination(:console)
-  end
-
-  c.before :suite do
-    hosts.each do |host|
-      unless ENV['RS_PROVISION'] == 'no' || ENV['BEAKER_provision'] == 'no'
-        begin
-          on host, 'puppet --version'
-        rescue
-          if host.is_pe?
-            install_pe
-          else
-            install_puppet(host)
-          end
-        end
-      end
-
-      # Install module and dependencies
-      puppet_module_install(source: proj_root, module_name: File.basename(proj_root).gsub(%r{^puppet-}, ''))
-
-      install_module_from_forge('puppet-epel', '>= 3.0.0 < 4.0.0') if fact('os.family') == 'RedHat'
-      install_module_from_forge('puppetlabs-apache', '>= 2.1.0 < 6.0.0')
-    end
-  end
+configure_beaker do |host|
+  install_module_from_forge('puppet-epel', '>= 3.0.1 < 4.0.0') if fact_on(host, 'os.name') == 'CentOS'
+  install_module_from_forge('puppetlabs-apache', '>= 5.7.0 < 7.0.0')
+  install_module_from_forge('puppetlabs-java', '>= 6.3.0 < 8.0.0')
 end
