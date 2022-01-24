e783618741f700dbfc75c7e68f460e4de979b743
Tim Meusel <tim@bastelfreak.de>
2022-01-24 15:28:27 +0100

    rubocop: change string concat to interpolation

--- lib/puppet/provider/rvm_gem/gem.rb
+++ lib/puppet/provider/rvm_gem/gem.rb
@@ -35,7 +35,7 @@ Puppet::Type.type(:rvm_gem).provide(:gem) do
                end
 
     if (name = hash[:justme])
-      command << ('^' + name + '$')
+      command << ("^#{name}$")
     end
 
     # use proxy if proxy_url is set
