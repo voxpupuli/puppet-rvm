ecb5d77d57015447f1eea4fabfc9fc4a6d6a72e4
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-07-06 16:10:05 +0200

    Remove unused variables

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,9 +5,6 @@ class rvm::params (
 ) {
   $group = 'rvm'
 
-  $proxy_url = undef
-  $no_proxy = undef
-
   $signing_keys = [
     { 'id' => 'D39DC0E3', 'source' => 'https://rvm.io/mpapis.asc' },
     { 'id' => '39499BDB', 'source' => 'https://rvm.io/pkuczynski.asc' },
