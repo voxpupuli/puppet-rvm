552a96131ed2342582213974199a5aa0e867425c
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-01-08 19:57:40 +0100

    Modulesync 7.2.0

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,7 +3,7 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  install_module_from_forge('puppet-epel', '>= 3.0.1 < 4.0.0') if fact_on(host, 'os.family') == 'RedHat'
-  install_module_from_forge('puppetlabs-apache', '>= 5.7.0 < 7.0.0')
-  install_module_from_forge('puppetlabs-java', '>= 6.3.0 < 8.0.0')
+  install_puppet_module_via_pmt_on(host, 'puppet-epel', '>= 3.0.1 < 4.0.0') if fact_on(host, 'os.family') == 'RedHat'
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-apache', '>= 5.7.0 < 7.0.0')
+  install_puppet_module_via_pmt_on(host, 'puppetlabs-java', '>= 6.3.0 < 8.0.0')
 end
