9ff97d826fe6c53df38b1c71011de3b7d7980f19
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-01-09 11:40:13 +0100

    Install puppet-epel in CI on the entire Red Hat OS family

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,7 +3,7 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  install_module_from_forge('puppet-epel', '>= 3.0.1 < 4.0.0') if fact_on(host, 'os.name') == 'CentOS'
+  install_module_from_forge('puppet-epel', '>= 3.0.1 < 4.0.0') if fact_on(host, 'os.family') == 'RedHat'
   install_module_from_forge('puppetlabs-apache', '>= 5.7.0 < 7.0.0')
   install_module_from_forge('puppetlabs-java', '>= 6.3.0 < 8.0.0')
 end
